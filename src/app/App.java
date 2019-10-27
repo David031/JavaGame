4f3cdfbeb632de34514dd8f3d4c7a2d07a60391c
David031 <davidtsang031@gmail.com>
2019-10-27 20:07:56 +0800

    add player

--- src/app/App.java
+++ src/app/App.java
@@ -1,76 +1,144 @@
 package app;
 
 public class App {
-    static String map1[][] = new String[15][15];
-    static int playerLocationX = 1;
-    static int playerLocationY = 1;
-    static Enemy boss;
+  static String map1[][] = new String[15][15];
+  static int  px;
+  static int  py;
+  static Enemy boss[] = new  Enemy[15];
+  static Enemy minion[][] = new Enemy[15][5];
 
-    public static void init() {
+  static void init() {
+     px = 1;
+     py = 1;
+     for (int i = 0; i < boss.length; i++) {
+       boss[i] = new Enemy(EnemyType.Boss, i+1);
+     }
+     for (int i = 0; i < minion.length; i++) {
+       for (int j = 0; j < minion[i].length; j++) {
+         minion[i][j] = new Enemy(EnemyType.Minion, i+1);
+       }
+     }
+    initMap1();
+    printBanner();
+  }
 
-        for (int i = 0; i < map1.length; i++) {
-            for (int j = 0; j < map1.length; j++) {
-                if (i == 0 || i == (map1.length - 1)) {
-                    map1[i][j] = "# ";
-                } else if (j == 0 || j == (map1.length - 1)) {
-                    map1[i][j] = "# ";
-                } else {
-                    map1[i][j] = "  ";
-                }
-            }
+  static void initMap1() {
+    for (int i = 0; i < map1.length; i++) {
+      for (int j = 0; j < map1.length; j++) {
+        if (i == 0 || i == (map1.length - 1)) {
+          map1[i][j] = "# ";
+        } else if (j == 0 || j == (map1.length - 1)) {
+          map1[i][j] = "# ";
+        } else {
+          map1[i][j] = "  ";
         }
+      }
     }
 
-    public static void playerMove(int x, int y) {
-        int oldx = playerLocationX;
-        int oldy = playerLocationY;
-        playerLocationX = x;
-        playerLocationY = y;
-        map1[oldx][oldy] = "  ";
-        map1[playerLocationX][playerLocationY] = "P ";
-    }
+  }
 
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
-    public static void controlPanel() {
-
-    }
-
-    public static void print(String string) {
-        System.out.println(string);
-    }
-
-    public static void printMap() {
-
-        for (int i = 0; i < map1.length; i++) {
-            for (int j = 0; j < map1.length; j++) {
-                System.out.print(map1[i][j]);
-            }
-            System.out.println();
+  static void initMap2() {
+    for (int i = 0; i < map1.length; i++) {
+      for (int j = 0; j < map1.length; j++) {
+        if (i == 0 || i == (map1.length - 1)) {
+          map1[i][j] = "# ";
+        } else if (j == 0 || j == (map1.length - 1)) {
+          map1[i][j] = "# ";
+        } else {
+          map1[i][j] = "  ";
         }
+      }
     }
+  }
 
-    public static void cScreenDelay() {
+  static void playerMove(int x, int y) {
+    int oldx =  px;
+    int oldy =  py;
+     px = x;
+     py = y;
+    map1[oldx][oldy] = "  ";
+    map1[ px][ py] = "P ";
+    cScreenDelay();
+  }
 
-        try {
-            Thread.sleep(1000);
-            clearScreen();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+  static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+
+  static void controlPanel() {
+
+  }
+
+  static void printMap(String[][] map) {
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map.length; j++) {
+        System.out.print(map[i][j]);
+      }
+      System.out.println();
     }
+  }
 
-    public static void main(String[] args) throws Exception {
-        init();
-        printMap();
-        playerMove(2, 2);
-        cScreenDelay();
-        printMap();
-        playerMove(8, 10);
-        cScreenDelay();
-        printMap();
+  static void cScreenDelay() {
+
+    try {
+      Thread.sleep(1000);
+      clearScreen();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
+
+  static void printBanner() {
+    System.out.println("    _/          _/  _/_/_/_/  _/        _/          _/_/_/    _/_/    _/      _/  _/_/_/_/");
+    System.out.println("    _/          _/  _/        _/        _/        _/        _/    _/  _/_/  _/_/  _/      ");
+    System.out.println("   _/    _/    _/  _/_/_/    _/        _/        _/        _/    _/  _/  _/  _/  _/_/_/   ");
+    System.out.println("    _/  _/  _/    _/        _/        _/        _/        _/    _/  _/      _/  _/        ");
+    System.out.println("     _/  _/      _/_/_/_/  _/_/_/_/  _/_/_/_/    _/_/_/    _/_/    _/      _/  _/_/_/_/   ");
+    cScreenDelay();
+    System.out.println("                                  _/_/_/_/_/    _/_/                                      ");
+    System.out.println("                                     _/      _/    _/                                     ");
+    System.out.println("                                    _/      _/    _/                                      ");
+    System.out.println("                                   _/      _/    _/                                       ");
+    System.out.println("                                  _/        _/_/                                          ");
+    cScreenDelay();
+    System.out.println("     _/_/_/    _/_/_/_/  _/_/_/_/  _/_/_/        _/_/_/      _/_/    _/_/_/    _/    _/   ");
+    System.out.println("     _/    _/  _/        _/        _/    _/      _/    _/  _/    _/  _/    _/  _/  _/     ");
+    System.out.println("    _/    _/  _/_/_/    _/_/_/    _/_/_/        _/    _/  _/_/_/_/  _/_/_/    _/_/        ");
+    System.out.println("   _/    _/  _/        _/        _/            _/    _/  _/    _/  _/    _/  _/  _/       ");
+    System.out.println(" _/_/_/    _/_/_/_/  _/_/_/_/  _/            _/_/_/    _/    _/  _/    _/  _/    _/       ");
+    cScreenDelay();
+  }
+
+  static void printStageClear() {
+    System.out.println("       _/_/_/  _/_/_/_/_/    _/_/      _/_/_/  _/_/_/_/        _/_/_/  _/        _/_/_/_/    _/_/    _/_/_/        _/  _/  ");
+    System.out.println("    _/            _/      _/    _/  _/        _/            _/        _/        _/        _/    _/  _/    _/      _/  _/   ");
+    System.out.println("     _/_/        _/      _/_/_/_/  _/  _/_/  _/_/_/        _/        _/        _/_/_/    _/_/_/_/  _/_/_/        _/  _/    ");
+    System.out.println("        _/      _/      _/    _/  _/    _/  _/            _/        _/        _/        _/    _/  _/    _/                 ");
+    System.out.println(" _/_/_/        _/      _/    _/    _/_/_/  _/_/_/_/        _/_/_/  _/_/_/_/  _/_/_/_/  _/    _/  _/    _/      _/  _/      ");
+    cScreenDelay();
+  }
+
+  static void printStart(){
+    System.out.println("    _/_/_/    _/_/_/_/    _/_/    _/_/_/    _/      _/       ");
+    System.out.println("   _/    _/  _/        _/    _/  _/    _/    _/  _/          ");
+    System.out.println("  _/_/_/    _/_/_/    _/_/_/_/  _/    _/      _/             ");
+    System.out.println(" _/    _/  _/        _/    _/  _/    _/      _/              ");
+    System.out.println("_/    _/  _/_/_/_/  _/    _/  _/_/_/        _/               ");
+    cScreenDelay();
+    System.out.println("       _/_/_/    _/_/        _/      _/       ");
+    System.out.println("    _/        _/    _/      _/      _/        ");
+    System.out.println("   _/  _/_/  _/    _/      _/      _/         ");
+    System.out.println("  _/    _/  _/    _/                          ");
+    System.out.println("   _/_/_/    _/_/        _/      _/           ");
+  }
+  public static void main(String[] args) throws Exception {
+    init();
+    // printStart();
+    // printMap(map1);
+    // playerMove(2, 2);
+    // printMap(map1);
+    // playerMove(8, 10);
+    // printMap(map1);
+  }
 }
--- src/app/Enemy.java
+++ src/app/Enemy.java
@@ -1,5 +1,61 @@
 package app;
 
 public class Enemy {
-     int hp = 10;
+     int hp;
+     int ad;
+     int ap;
+     int ar;
+     int mr;
+     int cr;
+     int x;
+     int y;
+     EnemyType enemyType;
+     int level;
+
+     Enemy(EnemyType type, int level) {
+          this.enemyType = type;
+          this.level = level;
+          switch (type) {
+          case Boss:
+               initBossAttributes();
+               break;
+          case Minion:
+               initMinionAttributes();
+               break;
+          default:
+               break;
+          }
+     }
+
+     void initBossAttributes() {
+          hp = 280 * level* (int) Math.round(Math.random() * 7);
+          ad = 200 * level* (int) Math.round(Math.random() * 2);
+          ap = 180 * level* (int) Math.round(Math.random() * 2);
+          ar = 130 * level* (int) Math.round(Math.random() * 2);
+          mr = 120 * level* (int) Math.round(Math.random() * 2);
+          cr = 2 * level* (int) Math.round(Math.random() * 2);
+          x = (int) Math.round(Math.random() * 15);
+          y = (int) Math.round(Math.random() * 15);
+     }
+
+     void initMinionAttributes() {
+          hp = 120 * level * (int) Math.round(Math.random() * 4);
+          ad = 100 * level * (int) Math.round(Math.random() * 3);
+          ap = 100 * level * (int) Math.round(Math.random() * 3);
+          ar = 100 * level * (int) Math.round(Math.random() * 3);
+          mr = 100 * level * (int) Math.round(Math.random() * 3);
+          cr = 4 * level;
+          x = (int) Math.round(Math.random() * 15);
+          y = (int) Math.round(Math.random() * 15);
+     }
+     void isCC(){
+
+     }
+     void isDead(){
+
+     }
+}
+
+enum EnemyType {
+     Boss, Minion,
 }
